51543d5c4400a28ca77a5555534d1b9bbc446caf
Krishna Sarma S <Krishna.Sarma@Honeywell.com>
2021-12-06 17:50:45 +0530

    feature branch update

--- Sample.java
+++ Sample.java
@@ -1,5 +1,10 @@
 public class Test{
 	public static void main(String args[]){
-		System.out.println("Hello");
+		System.out.println("Hello-Updated-1");
+		test();
+	}
+	
+	public static void test(){
+		System.out.println("Feature branch update");
 	}
 }
