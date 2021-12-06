3ad2ea04e9ac900bb2130ddffe43ebf50f4394fb
Krishna Sarma S <Krishna.Sarma@Honeywell.com>
2021-12-06 22:53:25 +0530

    f2 test1 updates

--- Sample.java
+++ Sample.java
@@ -2,9 +2,14 @@ public class Test{
 	public static void main(String args[]){
 		System.out.println("Hello-Update-rel2");
 		test();
+		test1();
 	}
 	
 	public static void test(){
 		System.out.println("Feature branch update");
+	}
+	
+	public static void test1(){
+		System.out.println("f2 test1 Feature branch update");
 	}
 }
